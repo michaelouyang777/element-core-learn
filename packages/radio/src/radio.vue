f5df4d8b18d07db6762a6d0227c9b862d65ad1a9
Michael <oy.xt@qq.com>
2021-06-03 09:32:30 +0800

    radio的说明

--- packages/radio/src/radio.vue
+++ packages/radio/src/radio.vue
@@ -14,13 +14,16 @@
     :tabindex="tabIndex"
     @keydown.space.stop.prevent="model = isDisabled ? model : label"
   >
+    <!-- radio 图标 -->
     <span class="el-radio__input"
       :class="{
         'is-disabled': isDisabled,
         'is-checked': model === label
       }"
     >
+      <!-- 图标效果 -->
       <span class="el-radio__inner"></span>
+      <!-- radio -->
       <input
         ref="radio"
         class="el-radio__original"
@@ -36,6 +39,7 @@
         tabindex="-1"
       >
     </span>
+    <!-- 文本内容 -->
     <span class="el-radio__label" @keydown.stop>
       <slot></slot>
       <template v-if="!$slots.default">{{label}}</template>
@@ -47,9 +51,9 @@
 
   export default {
     name: 'ElRadio',
-
+    // 使用mixin扩展组件
     mixins: [Emitter],
-
+    // 接收父级组件传递下来的数据（elementUI内部使用provide/inject作为父子组件的通讯方式）
     inject: {
       elForm: {
         default: ''
@@ -61,13 +65,19 @@
     },
 
     componentName: 'ElRadio',
-
+    // 定义可以传入的属性（elementUI使用props/$emit作为对外组件通讯的方式）
     props: {
+      // 绑定值
       value: {},
+      // Radio 的 value
       label: {},
+      // 是否禁用
       disabled: Boolean,
+      // 原生 name 属性
       name: String,
+      // 是否显示边框
       border: Boolean,
+      // Radio 的尺寸，仅在 border 为真时有效 【medium / small / mini】
       size: String
     },
 
@@ -77,6 +87,7 @@
       };
     },
     computed: {
+      // 向上遍历查询父级组件是否有 ElRadioGroup，即是否在按钮组中
       isGroup() {
         let parent = this.$parent;
         while (parent) {
@@ -89,6 +100,7 @@
         }
         return false;
       },
+      // 重新定义 v-model 绑定内容的 get 和 set
       model: {
         get() {
           return this.isGroup ? this._radioGroup.value : this.value;
@@ -102,26 +114,31 @@
           this.$refs.radio && (this.$refs.radio.checked = this.model === this.label);
         }
       },
+      // elFormItem 尺寸
       _elFormItemSize() {
         return (this.elFormItem || {}).elFormItemSize;
       },
+      // 计算 radio 尺寸，用于显示带有边框的radio的尺寸大小
       radioSize() {
         const temRadioSize = this.size || this._elFormItemSize || (this.$ELEMENT || {}).size;
         return this.isGroup
           ? this._radioGroup.radioGroupSize || temRadioSize
           : temRadioSize;
       },
+      // 是否禁用，如果radioGroup禁用则按钮禁用。
       isDisabled() {
         return this.isGroup
           ? this._radioGroup.disabled || this.disabled || (this.elForm || {}).disabled
           : this.disabled || (this.elForm || {}).disabled;
       },
+      // 标签索引 0 or -1
       tabIndex() {
         return (this.isDisabled || (this.isGroup && this.model !== this.label)) ? -1 : 0;
       }
     },
 
     methods: {
+      // 处理 @change 事件，如果有按钮组，出发按钮组事件。
       handleChange() {
         this.$nextTick(() => {
           this.$emit('change', this.model);
--- src/mixins/emitter.js
+++ src/mixins/emitter.js
@@ -1,31 +1,42 @@
+// 广播
 function broadcast(componentName, eventName, params) {
+  // 遍历子组件
   this.$children.forEach(child => {
+    // 组件名
     var name = child.$options.componentName;
 
     if (name === componentName) {
+      // 触发事件
       child.$emit.apply(child, [eventName].concat(params));
     } else {
+      // 执行broadcast方法
       broadcast.apply(child, [componentName, eventName].concat([params]));
     }
   });
 }
 export default {
   methods: {
+    // dispatch 方法向上获取父级组件并触发 eventName 事件
     dispatch(componentName, eventName, params) {
+      // 父级组件及其组件名
       var parent = this.$parent || this.$root;
       var name = parent.$options.componentName;
 
+      // 有父级组件 同时 没有name 或者 name 不等于组件名
       while (parent && (!name || name !== componentName)) {
+        // parent 向上获取父级组件
         parent = parent.$parent;
 
         if (parent) {
           name = parent.$options.componentName;
         }
       }
+      // 触发 eventName 事件
       if (parent) {
         parent.$emit.apply(parent, [eventName].concat(params));
       }
     },
+    // broadcast 方法向下遍历子组件触发 eventName 事件
     broadcast(componentName, eventName, params) {
       broadcast.call(this, componentName, eventName, params);
     }
